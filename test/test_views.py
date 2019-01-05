ec8e687eae2b8ab55feea11adf1bbe742742dc21
davidpique <kayongopique@gmail.com>
2019-01-05 09:23:43 -0800

    second commit

--- test/test_views.py
+++ test/test_views.py
@@ -10,25 +10,19 @@ class TestRedflag(unittest.TestCase):
      def setUp(self):
           self.app_tester=app.test_client(self)
           
-          self.incident1= {"createdby":"001",\
-          "createdon": "2018-09-12",\
+          self.incident1= {"username":"david",\
           "incidentType": "redflag",\
            "location": "gayaza", \
-           "status": "draft",\
           "comment": "please check it out"}
           
-          self.incident2= {"createdby":"001",\
-          "createdon": "2018-09-12",\
+          self.incident2= {"username":"davidpique",\
           "incidentType": "redflag",\
            "location": "kampala", \
-           "status": "draft",\
           "comment": "please check it out"}
           
-          self.incident3= {"createdby":"001",\
-          "createdon": "2018-09-12",\
+          self.incident3= {"username":"davidk",\
           "incidentType": "redflag",\
            "location": "entebbe", \
-           "status": "draft",\
           "comment": "please check it out"}
 
 
@@ -36,7 +30,7 @@ class TestRedflag(unittest.TestCase):
           input_data = {}
           response=self.app_tester.post('/api/v1/redflag', json=input_data )
           self.assertEqual(response.status_code, 404)
-          self.assertIn('bad request', str(response.data))
+          self.assertIn('Some fields are empty, please cross check', str(response.data))
 
      # def test_get_empty_redflag(self):
      #      input_data = {}
@@ -49,34 +43,34 @@ class TestRedflag(unittest.TestCase):
      def test_post_redflag(self):
           response = self.app_tester.post('/api/v1/redflag', json= self.incident1)
           self.assertEqual(response.status_code, 201)
-          self.assertIn('gayaza', str(response.data))
+          self.assertIn('created redflag', str(response.data))
 
      
 
      def test_get_redflag(self):
           res = self.app_tester.post('/api/v1/redflag', json= self.incident2)
           self.assertEqual(res.status_code, 201)
-          self.assertIn('kampala', str(res.data))
+          self.assertIn('created redflag', str(res.data))
           response = self.app_tester.get('/api/v1/redflag')
           self.assertEqual(response.status_code, 200)
           self.assertIn('please check it out', str(response.data))  
      
      
      def test_get_nonExistingSpecific_redflag(self):
-          response = self.app_tester.get('/api/v1/redflag/5')
+          response = self.app_tester.get('/api/v1/redflag/15')
           self.assertEqual(response.status_code, 404)
           self.assertIn('flag not found', str(response.data))   
 
      def test_get_specific_redflag(self):
           res = self.app_tester.post('/api/v1/redflag', json= self.incident2)
           self.assertEqual(res.status_code, 201)
-          self.assertIn('kampala', str(res.data))
+          self.assertIn('created redflag', str(res.data))
           response = self.app_tester.get('/api/v1/redflag/1')
           self.assertEqual(response.status_code, 200)
            
 
      def test_edit_nonExistingSpecific_redflagLocation(self):
-          response = self.app_tester.patch('/api/v1/redflag/7/location', json ={"location": "jinja"})
+          response = self.app_tester.patch('/api/v1/redflag/20/location', json ={"location": "jinja"})
           self.assertEqual(response.status_code, 404)
           self.assertIn('flag not found', str(response.data)) 
 
@@ -111,6 +105,30 @@ class TestRedflag(unittest.TestCase):
           response = self.app_tester.delete('/api/v1/redflag/1')
           self.assertEqual(response.status_code, 200)
           self.assertIn('redflag has been deleted', str(response.data))  
+
+     def test_update_status(self):
+          res = self.app_tester.post('/api/v1/redflag', json= self.incident3)
+          self.assertEqual(res.status_code, 201)
+          response = self.app_tester.patch('/api/v1/redflag/1/status', json ={"status": "rejected"})
+          self.assertEqual(response.status_code, 200)
+          self.assertIn('rejected', str(response.data))      
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
      
 
